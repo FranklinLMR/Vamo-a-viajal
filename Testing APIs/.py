fe2167c70c44371800d97d46c2f0c3534b98a003
FranklinLMR <rosariosanchezfranklin@gmail.com>
2026-03-10 08:41:50 -0400

    Apis

---
+++ Testing APIs/.py
@@ -0,0 +1,6 @@
+name= input("What country").strip()
+
+print(name.split())
+
+
+
--- Testing APIs/Rest_Countries.py
+++ Testing APIs/Rest_Countries.py
@@ -1,15 +1,60 @@
+
+
+name= "Dominican Republic".strip().split()
+
 import requests
 
-country = requests.get("https://restcountries.com/v3.1/all?fields=name,flags")
+namec= "%20".join(name)
+
+country = requests.get(f"https://restcountries.com/v3.1/name/{namec}?fullText=true&fields=name,capital,region,subregion,population,currencies,languages,flags,timezones,latlng")
 
 if country.status_code == 200:
-        Country_data= country.json()
+        Country_data= country.json()    
         
 
 else:
         
         print("this is not working")
 
+"""Country Data"""
+
+#Retrieve the whole dictionary:
+
+Country_data[0]
 
 
-print(Country_data[0])
+
+#Flag in png and svg
+
+Country_data[0]["flags"]["png"]
+
+Country_data[0]["flags"]["svg"]
+
+
+
+#CountryName
+
+Country_data[0]["name"]["common"]
+
+#This is for the long name
+
+Country_data[0]["name"]["official"]
+
+
+#Name    the country (original language)
+
+Country_data[0]["name"]["nativeName"]["spa"]["common"]   #We know what common and official mean
+
+Country_data[0]["name"]["nativeName"]["spa"]["official"]
+
+
+#Currency
+
+CurrencyCode= list(Country_data[0]["currencies"].keys())[0]
+
+CurrencyName= Country_data[0]["currencies"][CurrencyCode]["name"]
+
+
+# weather= requests.get(f"")
+
+
