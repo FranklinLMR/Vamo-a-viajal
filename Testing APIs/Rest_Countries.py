50ff3b59f4bfca724567f27a6c97478fc218c749
FranklinLMR <rosariosanchezfranklin@gmail.com>
2026-03-09 21:48:19 -0400

    Anamari, aca esta

--- Testing APIs/Rest_Countries.py
+++ Testing APIs/Rest_Countries.py
@@ -1,3 +1,15 @@
 import requests
 
+country = requests.get("https://restcountries.com/v3.1/all?fields=name,flags")
 
+if country.status_code == 200:
+        Country_data= country.json()
+        
+
+else:
+        
+        print("this is not working")
+
+
+
+print(Country_data[0])
--- main.py
+++ main.py
@@ -4,9 +4,29 @@ import requests
 
 def main(page: ft.Page):
     
-    pass
+    page.title= "Truth or Dare"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.bgcolor = "#f3f7ff"
+    page.update()
 
 
 
 
-ft.app(main)
+
+    plane_image= ft.Image(
+        src="assets/Plane.jpg", 
+        visible=True, 
+        top= 100,
+        left= 600,
+        width= 1080,
+        height= 720
+        )
+
+    Whole_page= ft.Stack([plane_image])
+    page.add(Whole_page)
+
+ft.run(main, assets_dir="assets")    
+
+
+
