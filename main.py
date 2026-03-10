c864995af5f0141c3af3b0a38536ae9adf2f9619
Anita <anamaridaviladejesus@gmail.com>
2026-03-10 11:17:32 -0400

    Added New Container for buttons

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import requests
 
 def main(page: ft.Page):
     
-    page.title= "Truth or Dare"
+    #fonts
+    page.fonts = {
+        "Main": "fredoka-latin-700-normal.ttf"
+    }
+
+    page.theme = ft.Theme(font_family="Main")
+    page.title= "Via Py"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.bgcolor = "#f3f7ff"
@@ -22,14 +28,44 @@ def main(page: ft.Page):
     logo_image = ft.Image(
         src="Logo.png",
         visible= True,
-        top = 100,
-        right=600,
-        width= 400
+        top = 35,
+        right = 700,
+        width= 700,
+        height=400
     )
 
+#buttons in main page
+    #blueContainer
+    blueButton = ft.Container(
+        bgcolor="#003d99",
+        height = 30,
+        width = 600,
+        border_radius=10
+        )
+    #whiteContainer
+    whiteButton = ft.Container(
+        bgcolor="#003d99",
+        height = 30,
+        width = 600,
+        border_radius=10
+        )
+    #images
+    canvaPlane = ft.Image(
+        src="Plane.jpg",
+        height = 25
+    )
+    #Text
+    textButton1 = ft.Text(
+        value="Book your flight!",
+        color="#000000",
+        size = 15
+    )
+
+    #stack for Book a flight
+    BookaFLight = ft.Stack([blueButton,whiteButton,canvaPlane,textButton1])
 
     Whole_page= ft.Stack([plane_image, logo_image])
-    page.add(Whole_page)
+    page.add(Whole_page, BookaFLight)
 
     
 
