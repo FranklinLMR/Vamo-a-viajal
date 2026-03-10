bebbc070d530d7e32a7b24e19800fd59aee0c01f
Anita <anamaridaviladejesus@gmail.com>
2026-03-10 10:51:24 -0400

    Add Logo

--- main.py
+++ main.py
@@ -4,27 +4,34 @@ import requests
 
 def main(page: ft.Page):
     
-    page.title= "flight"
+    page.title= "Truth or Dare"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.bgcolor = "#f3f7ff"
     page.update()
 
-
-
-
-
+#main page image
     plane_image= ft.Image(
         src="Plane.jpg", 
         visible=True, 
         top= 100,
-        left= 600,
-        width= 1080,
-        height= 720
+        left= 600
         )
 
-    Whole_page= ft.Stack([plane_image])
+#logo image
+    logo_image = ft.Image(
+        src="Logo.png",
+        visible= True,
+        top = 100,
+        right=600,
+        width= 400
+    )
+
+
+    Whole_page= ft.Stack([plane_image, logo_image])
     page.add(Whole_page)
+
+    
 
 ft.run(main, assets_dir="assets")    
 
