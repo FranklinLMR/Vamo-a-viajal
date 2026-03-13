9a73b42e6f55e204492862903678830603c2cae8
FranklinLMR <rosariosanchezfranklin@gmail.com>
2026-03-12 23:28:29 -0400

    Finally

--- secondpage.py
+++ secondpage.py
@@ -1,11 +1,16 @@
 import flet as ft
 import requests
- 
-import asyncio
 
 
+#Trying to PUT AN API OF VISAS< IM JUST GOING TO MAKE A LIST OF GENERAL CONTINENTAL INFORMATION
+#Ts is having me crazy
 
-
+RegionalVisaInfo= {"Americas": "From Canada to South America, entry rules vary a lot; \nmany Latin American countries are relatively flexible for regional visitors, \nwhile the U.S. and Canada usually require advance visas or electronic authorizations.",
+    "Europe": "Most European destinations are part of the Schengen Area; \nmany travelers need a Schengen visa obtained before arrival,\n though some nationalities get short-stay visa-free access.",
+    "Asia": "Asia mixes very easy and very strict countries;\n some offer visa-free or visa on arrival,\n others require detailed advance visa applications.",
+    "Africa": "Many African countries require a visa or eVisa before departure,\n and may ask for vaccinations,\n return tickets, and proof of accommodation.",
+    "Oceania": "Australia and New Zealand rely on electronic visas or travel authorizations,\n while many Pacific islands have more relaxed but still limited-stay entry rules.",
+}
 
 
 
@@ -44,7 +49,7 @@ def main(page: ft.Page):
 
         namec= "%20".join(name)
 
-        country = requests.get(f"https://restcountries.com/v3.1/name/{namec}?fullText=true&fields=name,capital,region,subregion,population,currencies,languages,flags,timezones,latlng")
+        country = requests.get(f"https://restcountries.com/v3.1/name/{namec}?fullText=true&fields=name,capital,region,subregion,population,currencies,languages,flags,timezones,latlng,cca2,cca3,idd")
 
         if country.status_code == 200:
             Country_data= country.json()    
@@ -90,6 +95,19 @@ def main(page: ft.Page):
 
             timezone =  Country_data[0]["timezones"][0]  #Some countries are very big
 
+
+
+            #Country Code
+
+            Ccode = Country_data[0]["cca3"]
+            C2code = Country_data[0]["cca2"]
+
+            #Number code
+
+
+
+            Noocode= Country_data[0]["idd"]["root"]
+
             #Weather information
 
 
@@ -100,19 +118,57 @@ def main(page: ft.Page):
 
             url = f"https://api.open-meteo.com/v1/forecast?latitude={Country_data[0]['latlng'][0]}&longitude={Country_data[0]['latlng'][1]}&daily=temperature_2m_min,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,precipitation_sum,precipitation_probability_max,weather_code,sunrise,sunset&timezone=auto"
             weather = requests.get(url)
-            data = weather.json()
+            Wdata = weather.json()
+            daily= Wdata["daily"]
 
 
+            lines=[]
 
+            for i in range(3):
+                date= daily["time"][i]
+                t_min= daily["temperature_2m_min"][i]
+                t_max= daily["temperature_2m_max"][i]
+                rainprec= daily["precipitation_sum"][i]
+                rainprob= daily["precipitation_probability_max"][i]
+                sunrise= daily["sunrise"][i][-5:]
+                sunset=daily["sunset"][i][-5:]
+                
+                line= (
+                     f"{date}: {t_min}-{t_max}°C, "
+                     f"rain {rainprec} mm ({rainprob}%), "
+                     f"sun {sunrise}-{sunset}"
+                )
+                lines.append(line)
+
+            #Local Time
+
+
+            LocaltimeAPI= requests.get(f"https://timeapi.io/api/v1/time/current/coordinate?latitude={Country_data[0]['latlng'][0]}&longitude={Country_data[0]['latlng'][1]}")
+
+
+            TimeData=LocaltimeAPI.json()
+
+
+            #Changing content
             Flag.content.src= flag_png
 
             NameAPI.value = NameCmmn
             CapAPI.value = capital
             RegAPI.value = region
             SRegAPI.value= subregion
+            PopuAPI.value =f"{population:,}"
+            CurreAPI.value= f"{CurrencyName} ({CurrencyCode})"
+            LanAPI.value= languageName
+            CCodeAPI.value= f"{Ccode}; No. Code: {Noocode}"
+            TZoneAPI.value= timezone
+            VisaAPI.value= f"Passport: {C2code}\nRegional info: {RegionalVisaInfo[region]}"
             
+            LocTimeAPI.value= f"{TimeData["time"][:-10]}"
 
 
+            #Weather Change
+
+            WeatherAPI.value= "\n".join(lines)
 
 
         else:
@@ -144,16 +200,16 @@ def main(page: ft.Page):
     Flag = ft.Container(ft.Image(src="FlagDR.png"), width=200)
     
     CName = ft.Text(value="Country Name", size = 20, color="#12366b")
-    NameAPI = ft.Text(value=f"", size = 15, color= "#000000")
+    NameAPI = ft.Text(value=f"Search a country", size = 15, color= "#000000")
  
     CapName = ft.Text(value="Capital City", size = 20, color="#12366b")
-    CapAPI = ft.Text(value=f"", size = 15, color= "#000000")
+    CapAPI = ft.Text(value=f"Search a country", size = 15, color= "#000000")
  
     Region = ft.Text(value="Region", size = 20, color="#12366b")
-    RegAPI = ft.Text(value=f"", size = 15, color= "#000000")
+    RegAPI = ft.Text(value=f"Search a country", size = 15, color= "#000000")
  
     SubRegion = ft.Text(value="Sub Region", size = 20, color="#12366b")
-    SRegAPI = ft.Text(value=f"", size = 15, color= "#000000")
+    SRegAPI = ft.Text(value=f"Search a country", size = 15, color= "#000000")
  
 #divider made by a container (im so smart i know) (Or maybe im just to lazy to investigate if there a vertical divider)
     divide = ft.Container(bgcolor="#000000", height=500, width=2)
@@ -172,13 +228,13 @@ def main(page: ft.Page):
  
     #column2
     Popu = ft.Text(value="Population", size = 30, color="#12366b")
-    PopuAPI = ft.Text(value=f"", size = 20, color= "#000000")
+    PopuAPI = ft.Text(value=f"Search a country", size = 20, color= "#000000")
  
     Curre = ft.Text(value="Currency", size = 30, color="#12366b")
-    CurreAPI = ft.Text(value=f" ()", size = 20, color= "#000000")
+    CurreAPI = ft.Text(value=f"Search a country ()", size = 20, color= "#000000")
  
     Lan = ft.Text(value="Language", size = 30, color="#12366b")
-    LanAPI = ft.Text(value=f"", size = 20, color= "#000000")
+    LanAPI = ft.Text(value=f"Search a country", size = 20, color= "#000000")
  
     column2 = ft.Column(
     controls=[
@@ -192,10 +248,10 @@ def main(page: ft.Page):
  
     #column3
     Visa = ft.Text(value="Basic Visa", size = 30, color="#12366b")
-    VisaAPI = ft.Text(value="Lorem Ipsum", size = 20, color= "#000000")
+    VisaAPI = ft.Text(value="Seach a country", size = 12    , color= "#000000")
  
     cCode = ft.Text(value="Country Code", size = 30, color="#12366b")
-    CCodeAPI = ft.Text(value="Lorem Ipsum", size = 20, color= "#000000")
+    CCodeAPI = ft.Text(value="Search a country", size = 20, color= "#000000")
  
     column3 = ft.Column(
     controls=[
@@ -208,13 +264,13 @@ def main(page: ft.Page):
  
     #column4 I forgot to add it. (Im so done teacher)
     Tzone = ft.Text(value="Time Zone", size = 30, color="#12366b")
-    TZoneAPI = ft.Text(value=f"", size = 20, color= "#000000")
+    TZoneAPI = ft.Text(value=f"Search a country", size = 20, color= "#000000")
  
     Weather = ft.Text(value="Weather", size = 30, color="#12366b")
-    WeatherAPI = ft.Text(value="Lorem Ipsum", size = 20, color= "#000000")
+    WeatherAPI = ft.Text(value="Search a country", size = 12, color= "#000000")
  
     LocTime = ft.Text(value="Local Time", size = 30, color="#12366b")
-    LocTimeAPI = ft.Text(value="Lorem Ipsum", size = 20, color= "#000000")
+    LocTimeAPI = ft.Text(value="Search a country    ", size = 20, color= "#000000")
  
     column4 = ft.Column(
         controls=[
