64586cea217500f4c69531f2dfac754ece07f405
FranklinLMR <rosariosanchezfranklin@gmail.com>
2026-03-12 20:33:53 -0400

    fixing

--- secondpage.py
+++ secondpage.py
@@ -1,21 +1,89 @@
 import flet as ft
 import requests
  
-
-
-userinput= "Dominican Republic"
-#
+userinput=""
 name= userinput.strip().split()
-
-import requests
 
 namec= "%20".join(name)
 
 country = requests.get(f"https://restcountries.com/v3.1/name/{namec}?fullText=true&fields=name,capital,region,subregion,population,currencies,languages,flags,timezones,latlng")
 
 if country.status_code == 200:
-        Country_data= country.json()    
+    Country_data= country.json()    
         
+
+    #NOTE , all data is not mandatory, I just put everything that the API and fields gave me, only the ones necessary,and if useful
+    #Retrieve the whole dictionary:
+
+    Country_data[0]
+
+
+
+    #Flag in png and svg
+
+    flag_png= Country_data[0]["flags"]["png"] 
+
+    flag_svg= Country_data[0]["flags"]["svg"]
+
+
+
+    #CountryName
+
+    NameCmmn= Country_data[0]["name"]["common"]
+
+    #This is for the long name
+
+    NameOff= Country_data[0]["name"]["official"]
+
+
+    #Name    the country (original language)
+
+    CodeNameCmmn= list(Country_data[0]["name"]["nativeName"].keys())[0]
+
+    OriginalNameCmmn= Country_data[0]["name"]["nativeName"][CodeNameCmmn]["common"]   #We know what common and official mean
+
+    OriginalNameOff=Country_data[0]["name"]["nativeName"][CodeNameCmmn]["official"]
+
+
+    #Currency
+
+    CurrencyCode= list(Country_data[0]["currencies"].keys())[0]
+
+    CurrencyName= Country_data[0]["currencies"][CurrencyCode]["name"]
+
+    CurrencySymbol= Country_data[0]["currencies"][CurrencyCode]["symbol"]
+
+
+    #Language
+    languagecode= list(Country_data[0]["languages"].keys())[0]
+
+    languageName= Country_data[0]["languages"][languagecode]
+
+
+    #More info
+
+    capital = Country_data[0]["capital"][0]
+
+    region=  Country_data[0]["region"]
+
+    subregion =  Country_data[0]["subregion"]
+
+    population=  Country_data[0]["population"]
+
+
+    timezones =  Country_data[0]["timezones"]
+
+    timezone =  Country_data[0]["timezones"][0]  #Some countries are very big
+
+    #Weather information
+
+
+    lat = Country_data[0]["latlng"][0]
+    lng = Country_data[0]["latlng"][1]
+
+    #Combining that data for retrieving  weather
+
+    # weather= requests.get(f"")
 
 else:
         
@@ -23,78 +91,7 @@ else:
 
 """Country Data"""
 
-#NOTE , all data is not mandatory, I just put everything that the API and fields gave me, only the ones necessary,and if useful
-#Retrieve the whole dictionary:
 
-Country_data[0]
-
-
-
-#Flag in png and svg
-
-flag_png= Country_data[0]["flags"]["png"] 
-
-flag_svg= Country_data[0]["flags"]["svg"]
-
-
-
-#CountryName
-
-NameCmmn= Country_data[0]["name"]["common"]
-
-#This is for the long name
-
-NameOff= Country_data[0]["name"]["official"]
-
-
-#Name    the country (original language)
-
-CodeNameCmmn= list(Country_data[0]["name"]["nativeName"].keys())[0]
-
-OriginalNameCmmn= Country_data[0]["name"]["nativeName"][CodeNameCmmn]["common"]   #We know what common and official mean
-
-OriginalNameOff=Country_data[0]["name"]["nativeName"][CodeNameCmmn]["official"]
-
-
-#Currency
-
-CurrencyCode= list(Country_data[0]["currencies"].keys())[0]
-
-CurrencyName= Country_data[0]["currencies"][CurrencyCode]["name"]
-
-CurrencySymbol= Country_data[0]["currencies"][CurrencyCode]["symbol"]
-
-
-#Language
-languagecode= list(Country_data[0]["languages"].keys())[0]
-
-languageName= Country_data[0]["languages"][languagecode]
-
-
-#More info
-
-capital = Country_data[0]["capital"][0]
-
-region=  Country_data[0]["region"]
-
-subregion =  Country_data[0]["subregion"]
-
-population=  Country_data[0]["population"]
-
-
-timezones =  Country_data[0]["timezones"]
-
-timezone =  Country_data[0]["timezones"][0]  #Some countries are very big
-
-#Weather information
-
-
-lat = Country_data[0]["latlng"][0]
-lng = Country_data[0]["latlng"][1]
-
-#Combining that data for retrieving  weather
-
-# weather= requests.get(f"")
 
 
 
@@ -133,16 +130,16 @@ def main(page: ft.Page):
     Flag = ft.Container(ft.Image(src="FlagDR.png"), width=200)
     
     CName = ft.Text(value="Country Name", size = 20, color="#12366b")
-    NameAPI = ft.Text(value=f"{NameCmmn}", size = 15, color= "#000000")
+    NameAPI = ft.Text(value=f"", size = 15, color= "#000000")
  
     CapName = ft.Text(value="Capital City", size = 20, color="#12366b")
-    CapAPI = ft.Text(value=f"{capital}", size = 15, color= "#000000")
+    CapAPI = ft.Text(value=f"", size = 15, color= "#000000")
  
     Region = ft.Text(value="Region", size = 20, color="#12366b")
-    RegAPI = ft.Text(value=f"{region}", size = 15, color= "#000000")
+    RegAPI = ft.Text(value=f"", size = 15, color= "#000000")
  
     SubRegion = ft.Text(value="Sub Region", size = 20, color="#12366b")
-    SRegAPI = ft.Text(value=f"{subregion}", size = 15, color= "#000000")
+    SRegAPI = ft.Text(value=f"", size = 15, color= "#000000")
  
 #divider made by a container (im so smart i know) (Or maybe im just to lazy to investigate if there a vertical divider)
     divide = ft.Container(bgcolor="#000000", height=500, width=2)
@@ -161,13 +158,13 @@ def main(page: ft.Page):
  
     #column2
     Popu = ft.Text(value="Population", size = 30, color="#12366b")
-    PopuAPI = ft.Text(value=f"{population}", size = 20, color= "#000000")
+    PopuAPI = ft.Text(value=f"", size = 20, color= "#000000")
  
     Curre = ft.Text(value="Currency", size = 30, color="#12366b")
-    CurreAPI = ft.Text(value=f"{CurrencyName} ({CurrencyCode})", size = 20, color= "#000000")
+    CurreAPI = ft.Text(value=f" ()", size = 20, color= "#000000")
  
     Lan = ft.Text(value="Language", size = 30, color="#12366b")
-    LanAPI = ft.Text(value=f"{languageName}", size = 20, color= "#000000")
+    LanAPI = ft.Text(value=f"", size = 20, color= "#000000")
  
     column2 = ft.Column(
     controls=[
@@ -197,7 +194,7 @@ def main(page: ft.Page):
  
     #column4 I forgot to add it. (Im so done teacher)
     Tzone = ft.Text(value="Time Zone", size = 30, color="#12366b")
-    TZoneAPI = ft.Text(value=f"{timezones[0]}", size = 20, color= "#000000")
+    TZoneAPI = ft.Text(value=f"", size = 20, color= "#000000")
  
     Weather = ft.Text(value="Weather", size = 30, color="#12366b")
     WeatherAPI = ft.Text(value="Lorem Ipsum", size = 20, color= "#000000")
